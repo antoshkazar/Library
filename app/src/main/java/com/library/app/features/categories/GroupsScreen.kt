7f19b6f1065f9b1b5e44d88f8805a6ef4ac5d6fa
anatoliyZorniy <Anat_zorniy@lenta.com>
2024-03-27 20:05:53 +0300

    bottomsheet

--- app/src/main/java/com/library/app/features/categories/GroupsScreen.kt
+++ app/src/main/java/com/library/app/features/categories/GroupsScreen.kt
@@ -13,12 +13,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -31,7 +37,10 @@ import com.library.presentation.composables.books.BookView
 import com.library.presentation.composables.containers.GroupItem
 import com.library.presentation.composables.containers.ScaffoldUI
 import com.library.presentation.composables.containers.Screen
+import com.library.presentation.composables.input.InputTextField
+import com.library.presentation.composables.input.InputTextFieldUI
 import com.library.presentation.theme.LibraryTypography
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
@@ -47,6 +56,15 @@ fun GroupsScreen(
     val subBooks by remember {
         viewModel.subBooks
     }
+
+    val newGroupName by remember {
+        viewModel.newGroupName
+    }
+
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showRegistrationBottomSheet by remember { mutableStateOf(false) }
+
     Screen(
         viewModel = viewModel,
         onScreenLaunch = viewModel::onScreenLaunch,
@@ -110,6 +128,43 @@ fun GroupsScreen(
                                     bookUi = bookUi,
                                     modifier = Modifier.clickable { viewModel.onBookClick(bookUi) })
                             }
+                        }
+                    }
+                }
+
+                if (showRegistrationBottomSheet) {
+                    ModalBottomSheet(
+                        modifier = Modifier,
+                        onDismissRequest = {
+                            showRegistrationBottomSheet = false
+                        },
+                        sheetState = sheetState
+                    ) {
+
+                        InputTextField(
+                            modifier = Modifier.padding(horizontal = 24.dp),
+                            uiData = InputTextFieldUI(
+                                text = newGroupName,
+                                label = stringResource(id = R.string.name)
+                            ),
+                            onValueChange = viewModel::onGroupNameChange,
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 24.dp),
+                            enabled = newGroupName.isNotEmpty(),
+                            onClick = {
+                                scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                    if (!sheetState.isVisible) {
+                                        showRegistrationBottomSheet = false
+                                    }
+                                }
+                                viewModel.onCreateGroupClick()
+                            }) {
+                            Text(text = stringResource(id = R.string.create))
                         }
                     }
                 }
